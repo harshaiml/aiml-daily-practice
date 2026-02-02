a95440bebd90e91a229a31e5a4ddec1dd720177c
Harsh Vardhan Kushwaha <harshvardhanaiml@gmail.com>
2026-02-02 23:16:17 +0530

    Adding new file model_training.ipynb

--- notebooks/API_demo/demo-api.py
+++ notebooks/API_demo/demo-api.py
@@ -1,2 +1,12 @@
-import flask
-print(flask.__version__)
+from flask import Flask
+import pickle
+
+app = Flask(__name__)
+
+@app.route("/ping",methods=['GET'])
+def ping():
+    return {"msg ":" This your first demo Flask" }
+
+@app.route("/harsh",methods=['GET'])
+def hello_harsh():
+    return "<p>Hello , Harsh</p>"
